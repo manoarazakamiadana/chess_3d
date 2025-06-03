4b63ef2b5ed1c781419ee4f7a8bd6fbdf6d1f446
manoarazakamiadana <manoarazakamiadana@gmail.com>
2025-06-03 10:01:07 +0200

    feat: :rocket: table finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { Canvas } from "@react-three/fiber";
+import Table from "./components/Table";
+import { OrbitControls } from "@react-three/drei";
+
+
 export default function () {
-  return <h1>Bonjour</h1>
+    return (
+        <Canvas
+            camera={{ position: [100, 100, 100] }}
+            style={{ width: "100vw", height: "100vh" }}
+        >
+            <OrbitControls />
+            <ambientLight />
+            <Table />
+        </Canvas>
+    )
 }
