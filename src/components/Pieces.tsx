d0f8a4104bab1252cf72968e749547888990db57
manoarazakamiadana <manoarazakamiadana@gmail.com>
2025-06-04 11:33:17 +0200

    feat: :bookmark: move suggestion

--- src/components/Pieces.tsx
+++ src/components/Pieces.tsx
@@ -1,12 +1,57 @@
-import PieceComponents from "./Piece";
 import type { Piece } from "../types"
+import { useGLTF } from "@react-three/drei";
+import { useEffect, useMemo } from "react";
+import { clone } from "three/examples/jsm/utils/SkeletonUtils.js";
+import type { Mesh, MeshStandardMaterial } from "three";
+import { caseSize } from "../constants";
+import { getSuggestion } from "../utils";
 
 const pieces : Piece[] = [
     {
         color: "white",
         position: {
-            x: 2,
-            y: -4
+            x: -4,
+            y: -3
+        },
+        type: "pawn"
+    },
+    {
+        color: "white",
+        position: {
+            x: -3,
+            y: -3
+        },
+        type: "pawn"
+    },
+    {
+        color: "white",
+        position: {
+            x: -2,
+            y: -3
+        },
+        type: "pawn"
+    },
+    {
+        color: "white",
+        position: {
+            x: -1,
+            y: -3
+        },
+        type: "pawn"
+    },
+    {
+        color: "white",
+        position: {
+            x: 0,
+            y: -3
+        },
+        type: "pawn"
+    },
+    {
+        color: "white",
+        position: {
+            x: 1,
+            y: -3
         },
         type: "pawn"
     },
@@ -21,55 +66,15 @@ const pieces : Piece[] = [
     {
         color: "white",
         position: {
-            x: 2,
-            y: -2
-        },
-        type: "pawn"
-    },
-    {
-        color: "white",
-        position: {
-            x: 2,
-            y: -1
-        },
-        type: "pawn"
-    },
-    {
-        color: "white",
-        position: {
-            x: 2,
-            y: 0
-        },
-        type: "pawn"
-    },
-    {
-        color: "white",
-        position: {
-            x: 2,
-            y: 1
-        },
-        type: "pawn"
-    },
-    {
-        color: "white",
-        position: {
-            x: 2,
-            y: 2
-        },
-        type: "pawn"
-    },
-    {
-        color: "white",
-        position: {
-            x: 2,
-            y: 3
-        },
-        type: "pawn"
-    },
-    {
-        color: "white",
-        position: {
             x: 3,
+            y: -3
+        },
+        type: "pawn"
+    },
+    {
+        color: "white",
+        position: {
+            x: -4,
             y: -4
         },
         type: "rook"
@@ -77,48 +82,48 @@ const pieces : Piece[] = [
     {
         color: "white",
         position: {
-            x: 3,
-            y: -3
+            x: -3,
+            y: -4
         },
         type: "knight"
     },
     {
         color: "white",
         position: {
-            x: 3,
-            y: -2
+            x: -2,
+            y: -4
         },
         type: "bishop"
     },
     {
         color: "white",
         position: {
-            x: 3,
-            y: -1
+            x: -1,
+            y: -4
         },
         type: "queen"
     },
     {
         color: "white",
         position: {
-            x: 3,
-            y: 0
+            x: 0,
+            y: -4
         },
         type: "king"
     },
     {
         color: "white",
         position: {
-            x: 3,
-            y: 1
+            x: 1,
+            y: -4
         },
         type: "bishop"
     },
     {
         color: "white",
         position: {
-            x: 3,
-            y: 2
+            x: 2,
+            y: -4
         },
         type: "knight"
     },
@@ -126,55 +131,15 @@ const pieces : Piece[] = [
         color: "white",
         position: {
             x: 3,
-            y: 3
+            y: -4
         },
         type: "rook"
     },
     {
         color: "black",
         position: {
-            x: -3,
-            y: -4
-        },
-        type: "pawn"
-    },
-    {
-        color: "black",
-        position: {
-            x: -3,
-            y: -3
-        },
-        type: "pawn"
-    },
-    {
-        color: "black",
-        position: {
-            x: -3,
-            y: -2
-        },
-        type: "pawn"
-    },
-    {
-        color: "black",
-        position: {
-            x: -3,
-            y: -1
-        },
-        type: "pawn"
-    },
-    {
-        color: "black",
-        position: {
-            x: -3,
-            y: 0
-        },
-        type: "pawn"
-    },
-    {
-        color: "black",
-        position: {
-            x: -3,
-            y: 1
+            x: -4,
+            y: 2
         },
         type: "pawn"
     },
@@ -189,8 +154,48 @@ const pieces : Piece[] = [
     {
         color: "black",
         position: {
-            x: -3,
-            y: 3
+            x: -2,
+            y: 2
+        },
+        type: "pawn"
+    },
+    {
+        color: "black",
+        position: {
+            x: -1,
+            y: 2
+        },
+        type: "pawn"
+    },
+    {
+        color: "black",
+        position: {
+            x: 0,
+            y: 2
+        },
+        type: "pawn"
+    },
+    {
+        color: "black",
+        position: {
+            x: 1,
+            y: 2
+        },
+        type: "pawn"
+    },
+    {
+        color: "black",
+        position: {
+            x: 2,
+            y: 2
+        },
+        type: "pawn"
+    },
+    {
+        color: "black",
+        position: {
+            x: 3,
+            y: 2
         },
         type: "pawn"
     },
@@ -198,62 +203,62 @@ const pieces : Piece[] = [
         color: "black",
         position: {
             x: -4,
-            y: -4
+            y: 3
         },
         type: "rook"
     },
     {
         color: "black",
         position: {
-            x: -4,
-            y: -3
+            x: -3,
+            y: 3
         },
         type: "knight"
     },
     {
         color: "black",
         position: {
-            x: -4,
-            y: -2
+            x: -2,
+            y: 3
         },
         type: "bishop"
     },
     {
         color: "black",
         position: {
-            x: -4,
-            y: -1
+            x: -1,
+            y: 3
         },
         type: "queen"
     },
     {
         color: "black",
         position: {
-            x: -4,
-            y: 0
+            x: 0,
+            y: 3
         },
         type: "king"
     },
     {
         color: "black",
         position: {
-            x: -4,
-            y: 1
+            x: 1,
+            y: 3
         },
         type: "bishop"
     },
     {
         color: "black",
         position: {
-            x: -4,
-            y: 2
+            x: 2,
+            y: 3
         },
         type: "knight"
     },
     {
         color: "black",
         position: {
-            x: -4,
+            x: 3,
             y: 3
         },
         type: "rook"
@@ -268,4 +273,32 @@ export default function () {
             )
         })}
     </>
+}
+
+function PieceComponents(props: Piece) {
+    const { scene } = useGLTF(`/${props.type}.glb`)
+    const clonedScene = useMemo(() => clone(scene), [scene])
+
+    useEffect(() => {
+        clonedScene.traverse((child) => {
+        if ((child as Mesh).isMesh) {
+            const mesh = child as Mesh
+            const material = mesh.material as MeshStandardMaterial
+            if (material && material.color) {
+            material.color.set(props.color == 'white' ? 0xbbbbbb : 0x444444)
+            }
+            mesh.material = material.clone()
+        }
+        })
+    }, [clonedScene])
+
+    return <primitive
+        object={clonedScene}
+        position={[props.color == "black" ? (props.position.x*caseSize)-caseSize/4 : (props.position.x*caseSize)+caseSize/4, 0, props.position.y*caseSize]}
+        scale={[caseSize*40, caseSize*40, caseSize*40]}
+        rotation={props.color == "black" ? [0, 0, 0] : [0, Math.PI, 0]}
+        onClick={function () {
+            console.log(getSuggestion(props, pieces))
+        }}
+    />
 }
